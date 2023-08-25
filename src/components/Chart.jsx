e10e3157259b82d2a38a4b7ed3c296e064897bcd
Vivek Patel <vpatel360@gmail.com>
2023-08-25 09:39:40 -0400

    chart options working

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,51 +2,91 @@ import React, { useState, useEffect, useMemo } from 'react'
 import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 import axios from 'axios'
-import chart_data from '../chart_data'
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 
 
 const Chart = ({ name, id }) => {
-    console.log(name, id)
 
     const [coinData, setCoinData] = useState(null)
-    const [formData, setFormData] = useState('Daily')
-    
-    console.log(formData)
+    const [formData, setFormData] = useState('Week')
+    const [chartData, setChartData] = useState(null)
+    const [apiInterval, setApiInterval] = useState({
+        interval: 'd1',
+        start: Date.now() - 691200000,
+        end: Date.now()
+    })
 
-    const handleChange = (e) =>{
-       setFormData(e.target.value)
+    console.log(coinData)
+
+    const handleChange = (e) => {
+        setFormData(() => e.target.value)
     }
-
+    
     useEffect(() => {
-        axios.get(`https://api.coincap.io/v2/assets/${id.toLowerCase()}/history?interval=d1&start=${Date.now() - 691200000}&end=${Date.now()}`)
+
+        if (formData === 'Week') {
+            setApiInterval({
+                interval: 'd1',
+                start: Date.now() - 691200000,
+                end: Date.now()
+            })
+        }
+        else if (formData === '24 hours') {
+            setApiInterval({
+                interval: 'h1',
+                start: Date.now() - 86400000,
+                end: Date.now()
+            })
+        }
+        else {
+            setApiInterval({
+                interval: 'm15',
+                start: Date.now() - 10800000,
+                end: Date.now()
+            })
+        }
+    }, [formData])
+    
+    useEffect(() => {
+        axios.get(`https://api.coincap.io/v2/assets/${id.toLowerCase()}/history?interval=${apiInterval.interval}&start=${apiInterval.start}&end=${apiInterval.end}`)
             .then(res => setCoinData(res.data.data))
             .catch(error => console.log('error', error));
-    }, [])
+    }, [apiInterval])
 
-
-    const chartData = useMemo(() => {
+    useEffect(() => {
         if (coinData === null) {
             return
         }
         else {
-            return {
-                labels: coinData.map(day => (new Date(day.time)).toLocaleDateString()),
-                datasets: [{
-                    label: 'Price',
-                    data: coinData.map(day => day.priceUsd)
-                }]
+            if (formData === 'Week') {
+                setChartData({...chartData,
+                    labels: coinData.map(day => (new Date(day.time)).toLocaleDateString()),
+                    datasets: [{
+                        label: 'Price',
+                        data: coinData.map(day => day.priceUsd)
+                    }]
+                }
+                )
+            }
+            else {
+                setChartData({ ...chartData,
+                    labels: coinData.map(day => (new Date(day.time)).toLocaleTimeString()),
+                    datasets: [{
+                        label: 'Price',
+                        data: coinData.map(day => day.priceUsd)
+                    }]
+                }
+                )
             }
         }
-    }
-        , [coinData])
+    }, [coinData])
 
 
     return (
-        <Row>
-            {coinData === null ?
+        <Row className='align-items-center'>
+            {chartData === null ?
                 <h2>Loading</h2>
                 :
                 <Col lg={9}>
@@ -55,11 +95,11 @@ const Chart = ({ name, id }) => {
             }
             <Col lg={3}>
                 <Form.Group className="mb-3 text-center">
-                    <Form.Label>Interval</Form.Label>
+                    <Form.Label>Past</Form.Label>
                     <Form.Select onChange={handleChange} className='bg-dark text-light'>
-                        <option >Daily</option>
-                        <option >Weekly</option>
-                        <option >Hourly</option>
+                        <option >Week</option>
+                        <option >24 hours</option>
+                        <option >3 hours</option>
                     </Form.Select>
                 </Form.Group>
             </Col>
--- src/components/MoreDetails.jsx
+++ src/components/MoreDetails.jsx
@@ -5,17 +5,18 @@ import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 
 const MoreDetails = ({coin, numberShortener}) => {
-    const { id, image, name, high_24h, low_24h } = coin
+    const { id, image, name, high_24h, low_24h, total_volume, price_change_24h } = coin
 
   return (
     <>
         <h3 className='text-center'>{name} <img style={{maxWidth:'2.5vw'}} src={image}/></h3>
-        <Row>
+        <Row className='align-items-center'>
             <Col lg={4}>
                  <Card style={{background:'rgb(65, 65, 105, 0.1)'}} className='text-light p-2 rounded border-0'>
-                    <p>Price Low (24hr): ${numberShortener(low_24h)}</p>
-                    <p>Price High (24hr): ${numberShortener(high_24h)}</p>
-
+                    <p style={{color:'#4655df'}}>Price Low (24hr): <span className='text-light'>${numberShortener(low_24h)}</span></p>
+                    <p style={{color:'#4655df'}}>Price High (24hr): <span className='text-light'>${numberShortener(high_24h)}</span></p>
+                    <p style={{color:'#4655df'}}>Volume (24hr): <span className='text-light'>${numberShortener(total_volume)}</span></p>
+                    <p style={{color:'#4655df'}}>Price Change (24hr): <span className='text-light'>${numberShortener(price_change_24h)}</span></p>
                  </Card>
             </Col>
             <Col lg={8}>
