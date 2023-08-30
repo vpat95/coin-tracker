331b076cdc3a33bc20ce67d28be1cd32e289e422
Vivek Patel <vpatel360@gmail.com>
2023-08-30 09:36:00 -0400

    adding default demo credentials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,16 @@
-import React,{useEffect, useRef} from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import { useAuth } from '../context/AuthContext'
 import {Link, useNavigate} from 'react-router-dom'
 import {Card, Form, Button, Container} from 'react-bootstrap'
 
 function Login() {
     const {user, loginUser} = useAuth()
+    const [login, setLoginInfo] = useState({
+        email: 'demo@demo.com',
+        password: 'demo1234'
+    })
     const navigate = useNavigate()
-    const emailRef = useRef(null)
-    const passwordRef = useRef(null)
+
 
     useEffect(() => {
         if(user){
@@ -15,10 +18,19 @@ function Login() {
         }
     },[])
 
+    function handleChange(e){
+        const name = e.target.name
+        const value = e.target.value
+        setLoginInfo({
+            ...login,
+            [name]:value
+        })
+    }
+
     function handleSubmit(e){
         e.preventDefault()
-        const email = emailRef.current.value
-        const password = passwordRef.current.value
+        const email = login.email
+        const password = login.password
 
         const userInfo = {email, password}
         loginUser(userInfo)
@@ -36,11 +48,11 @@ function Login() {
                         <Form className="text-light" onSubmit={handleSubmit}>
                             <Form.Group id="email">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" ref={emailRef} required/>
+                                <Form.Control onChange={handleChange} name='email' type="email" value={login.email} required/>
                             </Form.Group>
                             <Form.Group className="pt-2" id="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" ref={passwordRef} required/>
+                                <Form.Control onChange={handleChange} name='password' type="password" value={login.password} required/>
                             </Form.Group>
                             <Button type="submit" className='w-100 mt-3' style={{background: '#4655df', border:'none'}}>Log In</Button>
                         </Form>
